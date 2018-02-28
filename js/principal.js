70c244cecbc077c1f20d81297ab4d5a1d3bf5ac7
dpaula <fernando.dpaula@gmail.com>
2018-02-27 21:43:14 -0300

    pegando valores de uma tabela

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,16 @@
 var titulo = document.querySelector(".titulo");
 console.log(titulo);
 
-titulo.textContent = "Banana";
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdGordura = paciente.querySelector(".info-gordura");
+var gordura = tdGordura.textContent;
+
+var imc = peso / (altura*altura);
+
+console.log(imc);
