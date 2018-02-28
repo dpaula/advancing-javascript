082649b0d2a0f1a0e64f3e15d30c3251755721ab
dpaula <fernando.dpaula@gmail.com>
2018-02-27 21:58:21 -0300

    incluida validações

--- js/principal.js
+++ js/principal.js
@@ -16,4 +16,24 @@ var imc = peso / (altura*altura);
 var tdImc = paciente.querySelector(".info-imc");
 tdImc.textContent = imc;
 
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
 console.log(paciente);
