33048bc33ac39d06a8ced0017112a081b8b06889
dpaula <fernando.dpaula@gmail.com>
2018-03-03 13:50:38 -0300

    resgatando valores dos forms e criando campos novos

--- js/principal.js
+++ js/principal.js
@@ -37,4 +37,46 @@ for(var i = 0; i < pacientes.length ; i++){
         tdImc.textContent = imc.toFixed(2);
     }    
 } 
+
+//pegando o componente
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+//criando evento de click para o botão
+botaoAdicionar.addEventListener("click", function(event){
+    //anulando função default
+    event.preventDefault();
+    
+    //pegando o formulario
+    var formu = document.querySelector("#form-adiciona");
+
+    //extraindo valores do formulario
+    var nome = formu.nome.value;
+    var peso = formu.peso.value;
+    var altura = formu.altura.value;
+    var gordura = formu.gordura.value;
+
+    //criando uma tag tr
+    var pacienteTr = document.createElement("tr");
+
+    //criando tags tds
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    //incluindo os valores extraidos do form nas tags TDs
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    //conectando as tags td como filha da tr
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+});
     
