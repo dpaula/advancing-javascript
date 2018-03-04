be84e3df5f1fe1512aa4a6447f1e1d1d3245321c
dpaula <fernando.dpaula@gmail.com>
2018-03-04 20:42:11 -0300

    incluido evento na tabela, buscado o alvo do disparo e removida a lina

--- js/remove-paciente.js
+++ js/remove-paciente.js
@@ -1,9 +1,20 @@
 let pacients = document.querySelectorAll(".paciente");
-console.log(pacients);
+//console.log(pacients);
+
+var tabela = document.querySelector("#tabela-pacientes");
+//adiciona um evento para escutar o duplo clique na tabela
+tabela.addEventListener("dblclick", function (event) {
+    //busca o alvo do duplo clique
+    let alvoEvento = event.target;
+    //pega o componente pai do alvo clicado
+    let paiDoAlvo = alvoEvento.parentNode;
+    //remove o componennte (tr)
+    paiDoAlvo.remove();
+});
 
 //incluindo evento de duplo clique em um paciente, e removendo o mesmo
-pacients.forEach(paciente => {
-    paciente.addEventListener("dblclick", function () {
-        this.remove();
-    });
-}); 
+// pacients.forEach(paciente => {
+//     paciente.addEventListener("dblclick", function () {
+//         this.remove();
+//     });
+// }); 
